5e6c187f0cf713e6ebf2b9e520ed6c37b3c62fec
unknown <akhileshkumarlal7@gmail.com>
2025-07-31 01:10:03 +0530

    Switch to HashRouter and update config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import Dashboard from './pages/Dashboard/Dashboard'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-import Support from "./pages/Support/Support"
-import TransactionPage from './pages/TransactionPage/TransactionPage';
+// import React from "react";
+// import Dashboard from "./pages/Dashboard/Dashboard";
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import Support from "./pages/Support/Support";
+// import TransactionPage from "./pages/TransactionPage/TransactionPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard/>,
-  },
-  {
-    path: "/transaction",
-    element: <TransactionPage/>,
-  },
-  {
-    path: "/support",
-    element: <Support/>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Dashboard />,
+//   },
+//   {
+//     path: "/transaction",
+//     element: <TransactionPage />,
+//   },
+//   {
+//     path: "/support",
+//     element: <Support />,
+//   },
+// ]);
+
+// const App = () => {
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// };
+
+// export default App;
+import React from "react";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Support from "./pages/Support/Support";
+import TransactionPage from "./pages/TransactionPage/TransactionPage";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
-}
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transaction" element={<TransactionPage />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
